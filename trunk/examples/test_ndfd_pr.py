df3a738a3017fdc5ce67603484b5c6de4a818649
whitaker.jeffrey <whitaker.jeffrey@5f10a050-2530-0410-8541-f50d0bf955b6>
2007-05-14 18:43:21 +0000

    new tests for NDFD grids.

---
+++ trunk/examples/test_ndfd_pr.py
@@ -0,0 +1,34 @@
+from grib2 import Grib2Decode
+from pylab import *
+from matplotlib.toolkits.basemap import Basemap
+from numpy import ma
+grbs = Grib2Decode('../sampledata/dspr.temp.grb')
+g = grbs[3]
+data = g.data(masked_array=True)
+lats, lons = g.grid()
+llcrnrlon = lons[0,0]
+llcrnrlat = lats[0,0]
+urcrnrlon = lons[-1,-1]
+urcrnrlat = lats[-1,-1]
+rsphere = (g.earthRmajor,g.earthRminor)
+lat_ts = g.proj4_lat_ts
+lon_0 = g.proj4_lon_0
+projection = g.proj4_proj
+fig=figure()
+ax = fig.add_axes([0.1,0.1,0.75,0.75])
+m = Basemap(llcrnrlon=llcrnrlon,llcrnrlat=llcrnrlat,
+            urcrnrlon=urcrnrlon,urcrnrlat=urcrnrlat,rsphere=rsphere,lon_0=lon_0,
+            lat_ts=lat_ts,resolution='h',projection=projection)
+x,y = m(lons, lats)
+cs = m.contourf(x,y,data,20,cmap=cm.jet,colors=None)
+m.drawcoastlines()
+m.drawstates()
+m.drawcountries()
+m.drawmeridians(arange(280,300,1),labels=[0,0,0,1])
+m.drawparallels(arange(16,21,1),labels=[1,0,0,0])
+# new axis for colorbar.
+cax = axes([0.875, 0.10, 0.03, 0.75])
+colorbar(cs, cax, format='%d') # draw colorbar
+axes(ax)  # make the original axes current again
+title('NDFD Temp Puerto Rico '+g.forecast_time,fontsize=12)
+show()
