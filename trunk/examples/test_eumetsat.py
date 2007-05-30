48799312803f8aa88ecd205db33d2bb724ce0bbf
whitaker.jeffrey <whitaker.jeffrey@5f10a050-2530-0410-8541-f50d0bf955b6>
2007-05-30 21:18:52 +0000

    use projection='geos'

--- trunk/examples/test_eumetsat.py
+++ trunk/examples/test_eumetsat.py
@@ -1,23 +1,25 @@
 from grib2 import Grib2Decode
 from pylab import *
 from numpy import ma
-from matplotlib.toolkits.basemap import Basemap, addcyclic
+from matplotlib.toolkits.basemap import Basemap, cm
 
 grbs = Grib2Decode('../sampledata/eumetsat_precip.grb')
-fld = grbs[0].data(masked_array=True)
+g = grbs[0]
+fld = g.data(masked_array=True)
 print fld.shape, fld.min(), fld.max()
-lats, lons = grbs[0].grid()
-# make lons, lats into masked arrays.
-lons = ma.masked_values(lons, 1.e30)
-lats = ma.masked_values(lats, 1.e30)
-print lons.shape, lons.min(), lons.max()
-print lats.shape, lats.min(), lats.max()
+lats, lons = g.grid()
+print g.proj4_lon_0,g.proj4_h+g.earthRmajor
 
-m = Basemap(llcrnrlon=-180.,llcrnrlat=-90,urcrnrlon=180.,urcrnrlat=90.,\
-            resolution='c',area_thresh=10000.,projection='cyl')
+m = Basemap(lon_0=g.proj4_lon_0,satellite_height=g.proj4_h+g.earthRmajor,\
+            rsphere = (g.earthRmajor,g.earthRminor),\
+            resolution='l',area_thresh=10000.,projection='geos')
 # plot every 50th point.
-m.scatter(lons[::50,::50].flat,lats[::50,::50].flat,1,marker='o',color='k',zorder=10)
+x, y = m(lons,lats)
+m.scatter(x[::50,::50].flat,y[::50,::50].flat,1,marker='o',color='k',zorder=10)
 m.drawcoastlines()
-m.fillcontinents()
+m.fillcontinents(color='coral')
+m.drawparallels(arange(-80,81,20))
+m.drawmeridians(arange(-90,90,20))
+m.drawmapboundary()
 title('EUMETSAT geostationary projection grid')
 show()
