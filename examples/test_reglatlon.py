b22976bf106dbfd5f72d36f26e7b1e162ecaa123
whitaker.jeffrey <whitaker.jeffrey@5f10a050-2530-0410-8541-f50d0bf955b6>
2008-12-26 15:55:58 +0000

    update for scan mode changes.

--- examples/test_reglatlon.py
+++ examples/test_reglatlon.py
@@ -2,19 +2,25 @@ from grib2 import Grib2Decode
 from pylab import *
 from mpl_toolkits.basemap import Basemap
 grbs = Grib2Decode('../sampledata/gfs.grb')
-lats, lons = grbs[0].grid()
+for g in grbs:
+    if g.parameter_abbrev == 'PRES' and g.vertical_level_descriptor == 'Ground or Water Surface':
+        data = g.data()
+        lats,lons = g.grid()
+        break
 print lats[:,0]
 print lons[0,:]
 llcrnrlon = lons[0,0]
-llcrnrlat = lats[-1,-1]
+llcrnrlat = lats[0,0]
 urcrnrlon = lons[-1,-1]
-urcrnrlat = lats[0,0]
+urcrnrlat = lats[-1,-1]
 print llcrnrlon,llcrnrlat,urcrnrlon,urcrnrlat
 m = Basemap(llcrnrlon=llcrnrlon,llcrnrlat=llcrnrlat,
             urcrnrlon=urcrnrlon,urcrnrlat=urcrnrlat,
             resolution='c',projection='cyl')
 m.scatter(lons.flat,lats.flat,1,marker='o',color='k',zorder=10)
 m.drawcoastlines()
-m.fillcontinents()
+x,y = m(lons,lats)
+m.contourf(x,y,data,15)
+#m.fillcontinents()
 title('Global Lat/Lon Grid')
 show()
