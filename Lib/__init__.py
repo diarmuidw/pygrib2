61b6ff20ee4016c2011f69155ab2ddccdcf6ec48
whitaker.jeffrey <whitaker.jeffrey@5f10a050-2530-0410-8541-f50d0bf955b6>
2008-12-28 01:52:43 +0000

    regenerated docs

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -158,8 +158,8 @@ Changelog
    variable).  Inventory string now uses the abbreviation instead of the full
    parameter name. Added C{originating_center} instance variable.
  - B{%(__version__)s}: Bug fixes for scan mode handling, ensemble metadata, 
-   wave tables.  Masked arrays now returned by default is bitmap present..
-   Updated g2clib to version 1.1.7.
+   wave tables, satellite perspective projection.  Masked arrays now returned
+   by default is bitmap present Updated g2clib to version 1.1.7..
 
 @author: Jeffrey Whitaker.
 
