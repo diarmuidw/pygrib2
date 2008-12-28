16482801a94e07085b5b38a0d37f17a0425414e9
whitaker.jeffrey <whitaker.jeffrey@5f10a050-2530-0410-8541-f50d0bf955b6>
2008-12-28 01:50:49 +0000

    updated eumetsat example and data.

--- examples/test_eumetsat.py
+++ examples/test_eumetsat.py
@@ -6,8 +6,6 @@ from mpl_toolkits.basemap import Basemap, cm
 grbs = Grib2Decode('../sampledata/eumetsat_precip.grb')
 g = grbs[0]
 fld = g.data()
-print fld.shape, fld.min(), fld.max()
-print g.proj4_lon_0,g.proj4_h,g.earthRmajor,g.earthRminor
 lats, lons = g.grid()
 
 m = Basemap(lon_0=g.proj4_lon_0,satellite_height=g.proj4_h,\
@@ -17,14 +15,16 @@ m = Basemap(lon_0=g.proj4_lon_0,satellite_height=g.proj4_h,\
 x, y = m(lons,lats)
 m.scatter(x[::50,::50].flat,y[::50,::50].flat,1,marker='o',color='k',zorder=10)
 m.drawcoastlines()
-m.fillcontinents(color='coral')
+m.drawcountries()
+#m.fillcontinents(color='coral')
+m.drawcoastlines()
+# contour data.
+m.contourf(x,y,fld,20)
+# pcolor image (slower)
+#m.pcolor(x,y,fld)
 m.drawparallels(arange(-80,81,20))
 m.drawmeridians(arange(-90,90,20))
 m.drawmapboundary()
 title('EUMETSAT geostationary projection grid')
-
-figure()
-m.drawcoastlines()
-m.contourf(x,y,fld,20)
 
 show()
